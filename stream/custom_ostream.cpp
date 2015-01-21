882fd15b3f9b03d20ca79587011342ffe5ac2c64
wegatron <wegatron@gmail.com>
2015-01-21 23:13:10 +0800

    update

--- stream/custom_ostream.cpp
+++ stream/custom_ostream.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <streambuf>
+#include <fstream>
 #include <locale>
 #include <cstdio>
 #include <thread>
@@ -59,26 +60,34 @@ private:
     indentbuf ib;
 };
 
-class CompositOstream{
-
-  void addStream(std::ostream& os) {
-    oss_.push_back(os);
-  }
-
+class CompositOstream final {
+public:
+  CompositOstream() {}
   void clear() {
     oss_.clear();
   }
 
-  template<T>
-  CompositOstream &operator<<(const T &t)
-  {
-    for(std::ostream& os : oss_) {
-        os << t;
-      }
+  void addOstream(std::ostream* os) {
+    oss_.push_back(os);
   }
 
+  template <typename T>
+    CompositOstream& operator<<(const T& t) {
+    for(std::ostream* os : oss_) {
+      (*os) << t;
+    }
+    return *this;
+  }
+    CompositOstream& operator<<(std::ostream& (*__pf)(std::ostream&))
+    {
+      for(std::ostream* os : oss_) {
+          __pf(*os);
+        }
+        return *this;
+    }
 private:
-  std::vector<std::ostream> oss_;
+  CompositOstream(const CompositOstream& cls) {}
+  std::vector<std::ostream*> oss_;
 };
 
 int main()
@@ -86,5 +95,9 @@ int main()
 //    IndentStream is(cout);
 //    is << "31 hexadecimal: " << hex << 31 << endl;
 //    is << IndentStream::deindent << "31 hexadecimal: " << hex << 31 << endl;
+  CompositOstream test;
+  std::ofstream ofs ("test.txt", std::ofstream::out);
+  test.addOstream(&ofs);
+  test << 456 << " hahaha" << std::endl;
     return 0;
 }
